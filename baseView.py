7aece63c669547e84997eecf950383624a374d0e
DarkNukename <kardinallektor@gmail.com>
2020-09-09 00:39:07 +0300

    fix

--- baseView.py
+++ baseView.py
@@ -85,7 +85,7 @@ class BaseView(View):
         for token in ('access_token', 'refresh_token'):
             for key, value in fields.items():
                 print(key, value)
-                r.set('key'.encode('base64'), 'value'.encode('base64'))
+                r.set('key'.encode('idna'), 'value'.encode('idna'))
                 print(r.get('key'))
                 r.hset(tokens[token], key, str(value))
             r.expire(tokens[token], ttl[token])
