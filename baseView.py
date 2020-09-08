6332b660a570b99aff7f66c19daa1bae8e0d6991
DarkNukename <kardinallektor@gmail.com>
2020-09-08 23:37:46 +0300

    fix

--- baseView.py
+++ baseView.py
@@ -12,8 +12,7 @@ import base64
 
 from django.conf import settings
 
-
-r = redis.Redis() if settings.DEBUG else redis.Redis(os.environ.get("REDIS_URL").encode("idna"))
+r = redis.Redis() if settings.DEBUG else redis.Redis(base64.b64encode(os.environ.get("REDIS_URL").encode()).decode('ascii'))
 
 sentry_sdk.init(
     "https://e20a8a5dee99445f8917e97c3b39b260@o438046.ingest.sentry.io/5401287",
